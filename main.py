61a26fc4756bba13aa7cc70cbde5abdac950bec7
Michael Pfammatter <pfammatter@gmail.com>
2023-04-10 20:23:09 -0500

    Add scrolling word list and advance buttons

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ PLAY = asyncio.Event()
 CLOSE = False
 WORDS = []
 WORDS_LENGTH = 0
+WORD_INDEX = 0
+PARAGRAPH_INDEX = 0
 
-def CalculateDelayFromWPM(wpm:int) -> float:
+
+def CalculateDelayFromWPM(wpm: int) -> float:
     return 60.0 / float(wpm)
 
 
-async def windowRead(window:gui.Window):
-    global CLOSE
+async def windowRead(window: gui.Window):
+    global CLOSE, PARAGRAPH_INDEX, WORDS, WORDS_LENGTH, WORD_INDEX
     while True:
         await asyncio.sleep(0.01)
         event, values = window.read(0)
@@ -24,37 +27,89 @@ async def windowRead(window:gui.Window):
             break
         if event == "playPauseButton":
             print("play pause event")
-            if PLAY.is_set():
-                PLAY.clear()
-                window["playPauseButton"].update("Play")
-            else:
-                PLAY.set()
-                window["playPauseButton"].update("Pause")
-            window.Refresh()
-            
+            playPause(window)
+        if event == "pasteButton":
+            print("paste")
+            WORDS, WORDS_LENGTH = textClean(clipboard.paste())
+            WORD_INDEX = 0
+            PARAGRAPH_INDEX = 0
+            playPause(window, forcePlay=True)
+        if event == "cursorBeginning":
+            PARAGRAPH_INDEX = 0
+            WORD_INDEX = 0
+        if event == "cursorEnd":
+            PARAGRAPH_INDEX = len(WORDS)
+            WORD_INDEX = len(WORDS[PARAGRAPH_INDEX])
+        if event == "cursorPrevious":
+            targetIndex = WORD_INDEX - 1
+            if targetIndex < 0:
+                if PARAGRAPH_INDEX == 0:
+                    WORD_INDEX = 0
+                else:
+                    PARAGRAPH_INDEX -= 1
+                    WORD_INDEX = len(WORDS[PARAGRAPH_INDEX])
+        if event == "cursorNext":
+            targetIndex = WORD_INDEX + 1
+            if targetIndex > WORDS[PARAGRAPH_INDEX]:
+                if PARAGRAPH_INDEX != len(WORDS):
+                    PARAGRAPH_INDEX += 1
+                    WORD_INDEX = 0
+        if event.startswith("cursor"):
+            playPause(window, forcePause=True)
+
+
     CLOSE = True
     window.close()
     return
 
 
-async def updateWordDisplay(window:gui.Window) -> None:
-    paragraphIndex = 0
-    wordIndex = 0
-    for word in WORDS:
+async def updateWordDisplay(window: gui.Window) -> None:
+    global PARAGRAPH_INDEX, WORDS, WORDS_LENGTH, WORD_INDEX
+    while True:
         if CLOSE:
             break
         await wordAdvance()
-        window["-OUTPUT-"].update(word)
-        window.refresh()
-        print(word)
-        wordIndex += 1
+        words = WORDS[PARAGRAPH_INDEX]
+        if WORD_INDEX < len(words):
+            word = words[WORD_INDEX]
+            if WORD_INDEX > 0:
+                targetIndex = WORD_INDEX - 10
+                actualIndex = targetIndex if targetIndex > 0 else WORD_INDEX
+                preWords = " ".join(words[0:actualIndex])
+            else:
+                preWords = []
+            if WORD_INDEX < len(words) - 1:
+                targetIndex = WORD_INDEX + 10
+                actualIndex = (
+                    targetIndex if targetIndex < len(words) else WORD_INDEX + 1
+                )
+                postWords = " ".join(words[WORD_INDEX + 1 : actualIndex])
+            else:
+                postWords = []
+            if word.endswith("\n"):
+                await wordAdvance()
+            window["-OUTPUT-"].update(word)
+            window["wordStreamPre"].update(preWords)
+            window["wordStreamCurrent"].update(word)
+            window["wordStreamPost"].update(postWords)
+            window.refresh()
+            WORD_INDEX += 1
+        else:
+            PARAGRAPH_INDEX += 1
+            WORD_INDEX = 0
+            if PARAGRAPH_INDEX >= len(WORDS):
+                print("Paragraph Done")
+                PARAGRAPH_INDEX = 0
+                WORD_INDEX = 0
+                playPause(window, forcePause=True)
+            else:
+                await wordAdvance()
         if CLOSE:
             break
-        paragraphIndex += 1
     return
 
 
-async def wordAdvance(wpm:float=None) -> None:
+async def wordAdvance(wpm: float = None) -> None:
     wpm = wpm or 250
     delay = CalculateDelayFromWPM(wpm)
     await asyncio.sleep(delay)
@@ -64,43 +119,63 @@ async def wordAdvance(wpm:float=None) -> None:
     return
 
 
-async def main(window:gui.Window):
+async def main(window: gui.Window):
     await asyncio.gather(windowRead(window), updateWordDisplay(window))
     return
 
+
+def playPause(window: gui.Window, forcePlay=False, forcePause=False):
+    if forcePause:
+        play = True
+    elif forcePlay:
+        play = False
+    else:
+        play = PLAY.is_set()
+    if play:
+        PLAY.clear()
+        window["playPauseButton"].update("Play")
+    else:
+        PLAY.set()
+        window["playPauseButton"].update("Pause")
+    window.Refresh()
+
+
+def textClean(text: str):
+    words = [words.split() for words in text.splitlines()]
+    return words, len(words)
+
+
 if __name__ == "__main__":
     layout = [
-        [gui.Text("Ready?", font=("Any", 64, "bold"),key="-OUTPUT-")],
-        [gui.Button("Paste", key="pasteButton")],
-        [gui.Button("Play", key="playPauseButton")],
-        [gui.Button("Close")]
+        [gui.Text("Ready?", font=("Any", 64, "bold"), key="-OUTPUT-")],
+        [
+            gui.Text("press", key="wordStreamPre", pad=((5, 0), 3)),
+            gui.Text("play", key="wordStreamCurrent", pad=(0, 3)),
+            gui.Text("to begin", key="wordStreamPost", pad=((0, 5), 3)),
+        ],
+        [
+            gui.Button("Paste", key="pasteButton"),
+            gui.Button("Close"),
+        ],
+        [
+            gui.Button("|<", key="cursorBegining"),
+            gui.Button("<", key="cursorPrevious"),
+            gui.Button("Play", key="playPauseButton"),
+            gui.Button(">", key="cursorNext"),
+            gui.Button(">|", key="cursorEnd"),
+        ],
     ]
     window = gui.Window(title="So Many Words", layout=layout, finalize=True)
     window.bind("<space>", "playPauseButton")
+    window.bind("<Ctrl><v>", "pasteButton")
 
     text = (
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor "
-        "incididunt ut labore et dolore magna aliqua. Varius duis at consectetur lorem "
-        "donec massa. Phasellus faucibus scelerisque eleifend donec pretium vulputate. "
-        "Nulla facilisi nullam vehicula ipsum a. Sem nulla pharetra diam sit amet nisl "
-        "suscipit. Id interdum velit laoreet id donec ultrices tincidunt. Vulputate "
-        "dignissim suspendisse in est ante in nibh mauris cursus. Ultricies lacus sed "
-        "turpis tincidunt id aliquet risus feugiat in. Curs us sit amet dictum sit amet. "
-        "Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum. Orci sagittis "
-        "eu volutpat odio. Bibendum neque egestas congue quisque egestas diam.\n"
-        "Lacus vestibulum sed arcu non odio. Dignissim enim sit amet venenatis urna cursus "
-        "eget.  Porta non pulvinar neque laoreet suspendisse interdum. Tristique nulla "
-        "aliquet enim tortor at. Enim neque volutpat ac tincidunt vitae semper quis lectus "
-        "nulla. Et odio pellentesque diam volutpat commodo sed egestas. Tortor at auctor "
-        "urna nunc id cursus metus aliquam. Id velit ut tortor pretium viverra suspendisse "
-        "potenti nullam ac. Risus quis varius quam quisque id diam vel. Ultricies tristique "
-        "nulla aliquet enim. Vulputate sapien nec sagittis aliquam malesuada bibendum. "
-        "Risus at ultrices mi tempus imperdiet nulla malesuada pellentesque. Feugiat in "
-        "fermentum posuere urna nec. Sagittis vitae et leo duis ut diam quam. "
+        "This utility will display words to you at the specified words per minute "
+        "(WPM).\nThis is currently set to {WPM}.\nCopy text to the clipboard and hit "
+        'the "Paste" button.'
     )
     clip = clipboard.paste()
     # text = clip if type(clip) is str else text
-
     # WORDS = re.findall(r'\b\w+\b', text)
-    WORDS = text.split()
+    WORDS, WORDS_LENGTH = textClean(text)
     asyncio.run(main(window))
