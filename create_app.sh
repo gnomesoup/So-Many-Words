c93af0b2cbea807a6724922c989099626a2eb6b7
Michael Pfammatter <pfammatter@gmail.com>
2023-04-14 17:10:24 -0500

    Icons and build info

--- create_app.sh
+++ create_app.sh
@@ -1,8 +1,8 @@
 mkdir icon.iconset
-/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 16 -h 16 -o icon.iconset/icon_16x16.png icon.svg
-/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 32 -h 32 -o icon.iconset/icon_16x16@2x.png icon.svg
-/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 32 -h 32 -o icon.iconset/icon_32x32.png icon.svg
-/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 64 -h 64 -o icon.iconset/icon_32x32@2x.png icon.svg
+/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 16 -h 16 -o icon.iconset/icon_16x16.png icon_small.svg
+/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 32 -h 32 -o icon.iconset/icon_16x16@2x.png icon_small.svg
+/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 32 -h 32 -o icon.iconset/icon_32x32.png icon_small.svg
+/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 64 -h 64 -o icon.iconset/icon_32x32@2x.png icon_small.svg
 /Applications/Inkscape.app/Contents/MacOS/Inkscape -w 128 -h 128 -o icon.iconset/icon_128x128.png icon.svg
 /Applications/Inkscape.app/Contents/MacOS/Inkscape -w 256 -h 256 -o icon.iconset/icon_128x128@2x.png icon.svg
 /Applications/Inkscape.app/Contents/MacOS/Inkscape -w 256 -h 256 -o icon.iconset/icon_256x256.png icon.svg
@@ -12,4 +12,8 @@ mkdir icon.iconset
 iconutil -c icns icon.iconset
 rm -R icon.iconset
 
-pyinstaller --icon icon.icns --windowed --name 'So Many Words' --noconfirm main.py
+/Applications/Inkscape.app/Contents/MacOS/Inkscape -w 1028 -h 1028 -o icon.png icon.svg
+
+pyinstaller --icon icon.icns --windowed --onefile --name 'So Many Words' --noconfirm --add-data "./icon.png:." main.py
+rm -Rf "./dist/So Many Words"
+hdiutil create -srcfolder dist "So Many Words v2023.03.0.dmg"
